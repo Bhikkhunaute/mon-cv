1457ec5ac4a035b4e9f81b234e81ddc8b2566c33
Victor Prudhon <victor@air-de-victor.home>
2024-03-06 21:39:49 +0100

    Init commit

---
+++ src/components/InformationsPersonnelles.vue
@@ -0,0 +1,25 @@
+<!-- InformationsPersonnelles.vue -->
+
+<template>
+    <div class="informations-personnelles">
+      <h2>Informations Personnelles</h2>
+      <p>{{ nom }}</p>
+      <p>{{ num }}</p>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'InformationsPersonnelles',
+  props: {
+    nom: String,
+    num: Number
+  }
+}
+</script>
+
+<style scoped>
+  /* Styles CSS spécifiques à la section des informations personnelles */
+  .informations-personnelles {
+color: blueviolet;  }
+  </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <InformationsPersonnelles nom="Victor" num="0329293"/>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import InformationsPersonnelles from '@/components/InformationsPersonnelles.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    InformationsPersonnelles
+  }
+}
+</script>
